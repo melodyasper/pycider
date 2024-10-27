1684086bcb25808156441a88968c5b88d2198d3d
Melody Asper <melodyaheath@gmail.com>
2024-10-27 10:47:41 +0100

    Restructure

--- pycider/deciders.py
+++ pycider/deciders.py
@@ -76,133 +76,6 @@ class Decider(ABC, Generic[E, C, S]):
         pass
 
 
-I = TypeVar("I")  # identifier
-
-
-# Decider[E]
-# ManyDecider[tuple[I, E]] -> Decider[E]
-
-
-class ManyDecider(
-    Decider[tuple[I, E], tuple[I, C], MutableMapping[I, S]], Generic[I, E, C, S]
-):
-    def __init__(self, aggregate: type[Decider[E, C, S]]) -> None:
-        super().__init__()
-        self.aggregate = aggregate
-
-    def evolve(
-        self, state: MutableMapping[I, S], event: tuple[I, E]
-    ) -> MutableMapping[I, S]:
-
-        identifier = event[0]
-        current_event = event[1]
-
-        current_state = state.get(identifier)
-        if current_state is None:
-            current_state = self.aggregate().initial_state()
-
-        current_state = self.aggregate().evolve(current_state, current_event)
-        state[identifier] = current_state
-
-        return state
-
-    def decide(
-        self, command: tuple[I, C], state: MutableMapping[I, S]
-    ) -> Sequence[tuple[I, E]]:
-        identifier = command[0]
-        current_command = command[1]
-
-        current_state = state.get(identifier)
-        if current_state is None:
-            current_state = self.aggregate().initial_state()
-
-        events = list(
-            map(
-                lambda event: (identifier, event),
-                self.aggregate().decide(current_command, current_state),
-            )
-        )
-        return events
-
-    def is_terminal(self, state: MutableMapping[I, S]) -> bool:
-        for member_state in state.values():
-            if not self.aggregate().is_terminal(member_state):
-                return False
-        return True
-
-    def initial_state(self) -> MutableMapping[I, S]:
-        return {}
-
-
-CX = TypeVar("CX")
-EX = TypeVar("EX")
-SX = TypeVar("SX")
-CY = TypeVar("CY")
-EY = TypeVar("EY")
-SY = TypeVar("SY")
-
-TA = TypeVar("TA")
-TB = TypeVar("TB")
-
-
-class Left(Generic[TA]):
-    __match_args__ = ("value",)
-
-    def __init__(self, value: TA) -> None:
-        self.value = value
-
-
-class Right(Generic[TB]):
-    __match_args__ = ("value",)
-
-    def __init__(self, value: TB) -> None:
-        self.value = value
-
-
-Either = Left[TA] | Right[TB]
-
-
-class ComposeDecider(Generic[EX, CX, SX, EY, CY, SY]):
-    @classmethod
-    def compose(
-        cls, dx: Decider[EX, CX, SX], dy: Decider[EY, CY, SY]
-    ) -> Decider[Either[EX, EY], Either[CX, CY], tuple[SX, SY]]:
-        class AnonymousDecider(Decider[Either[EX, EY], Either[CX, CY], tuple[SX, SY]]):
-            def decide(
-                self, command: Either[CX, CY], state: tuple[SX, SY]
-            ) -> Sequence[Either[EX, EY]]:
-                match command:
-                    case Left():
-                        return list(
-                            map(lambda v: Left(v), dx.decide(command.value, state[0]))
-                        )
-                    case Right():
-                        return list(
-                            map(lambda v: Right(v), dy.decide(command.value, state[1]))
-                        )
-                    case _:
-                        raise RuntimeError("Type not implemented")
-
-            def evolve(
-                self, state: tuple[SX, SY], event: Left[EX] | Right[EY]
-            ) -> tuple[SX, SY]:
-                match event:
-                    case Left():
-                        return (dx.evolve(state[0], event.value), state[1])
-                    case Right():
-                        return (state[0], dy.evolve(state[1], event.value))
-                    case _:
-                        raise RuntimeError("Type not implemented")
-
-            def initial_state(self) -> tuple[SX, SY]:
-                return (dx.initial_state(), dy.initial_state())
-
-            def is_terminal(self, state: tuple[SX, SY]) -> bool:
-                return dx.is_terminal(state[0]) and dy.is_terminal(state[1])
-
-        return AnonymousDecider()
-
-
 class CatCommandWakeUp(Command):
     pass
 
@@ -343,3 +216,126 @@ class Bulb(Decider[Event, Command, State]):
 
     def is_terminal(self, state: State) -> bool:
         return isinstance(state, BulbStateBlown)
+
+
+I = TypeVar("I")  # identifier
+
+
+class ManyDecider(
+    Decider[tuple[I, E], tuple[I, C], MutableMapping[I, S]], Generic[I, E, C, S]
+):
+    def __init__(self, aggregate: type[Decider[E, C, S]]) -> None:
+        super().__init__()
+        self.aggregate = aggregate
+
+    def evolve(
+        self, state: MutableMapping[I, S], event: tuple[I, E]
+    ) -> MutableMapping[I, S]:
+
+        identifier = event[0]
+        current_event = event[1]
+
+        current_state = state.get(identifier)
+        if current_state is None:
+            current_state = self.aggregate().initial_state()
+
+        current_state = self.aggregate().evolve(current_state, current_event)
+        state[identifier] = current_state
+
+        return state
+
+    def decide(
+        self, command: tuple[I, C], state: MutableMapping[I, S]
+    ) -> Sequence[tuple[I, E]]:
+        identifier = command[0]
+        current_command = command[1]
+
+        current_state = state.get(identifier)
+        if current_state is None:
+            current_state = self.aggregate().initial_state()
+
+        events = list(
+            map(
+                lambda event: (identifier, event),
+                self.aggregate().decide(current_command, current_state),
+            )
+        )
+        return events
+
+    def is_terminal(self, state: MutableMapping[I, S]) -> bool:
+        for member_state in state.values():
+            if not self.aggregate().is_terminal(member_state):
+                return False
+        return True
+
+    def initial_state(self) -> MutableMapping[I, S]:
+        return {}
+
+
+CX = TypeVar("CX")
+EX = TypeVar("EX")
+SX = TypeVar("SX")
+CY = TypeVar("CY")
+EY = TypeVar("EY")
+SY = TypeVar("SY")
+
+TA = TypeVar("TA")
+TB = TypeVar("TB")
+
+
+class Left(Generic[TA]):
+    __match_args__ = ("value",)
+
+    def __init__(self, value: TA) -> None:
+        self.value = value
+
+
+class Right(Generic[TB]):
+    __match_args__ = ("value",)
+
+    def __init__(self, value: TB) -> None:
+        self.value = value
+
+
+Either = Left[TA] | Right[TB]
+
+
+class ComposeDecider(Generic[EX, CX, SX, EY, CY, SY]):
+    @classmethod
+    def compose(
+        cls, dx: Decider[EX, CX, SX], dy: Decider[EY, CY, SY]
+    ) -> Decider[Either[EX, EY], Either[CX, CY], tuple[SX, SY]]:
+        class AnonymousDecider(Decider[Either[EX, EY], Either[CX, CY], tuple[SX, SY]]):
+            def decide(
+                self, command: Either[CX, CY], state: tuple[SX, SY]
+            ) -> Sequence[Either[EX, EY]]:
+                match command:
+                    case Left():
+                        return list(
+                            map(lambda v: Left(v), dx.decide(command.value, state[0]))
+                        )
+                    case Right():
+                        return list(
+                            map(lambda v: Right(v), dy.decide(command.value, state[1]))
+                        )
+                    case _:
+                        raise RuntimeError("Type not implemented")
+
+            def evolve(
+                self, state: tuple[SX, SY], event: Left[EX] | Right[EY]
+            ) -> tuple[SX, SY]:
+                match event:
+                    case Left():
+                        return (dx.evolve(state[0], event.value), state[1])
+                    case Right():
+                        return (state[0], dy.evolve(state[1], event.value))
+                    case _:
+                        raise RuntimeError("Type not implemented")
+
+            def initial_state(self) -> tuple[SX, SY]:
+                return (dx.initial_state(), dy.initial_state())
+
+            def is_terminal(self, state: tuple[SX, SY]) -> bool:
+                return dx.is_terminal(state[0]) and dy.is_terminal(state[1])
+
+        return AnonymousDecider()
